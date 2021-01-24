ec90f9a273c9b736899dc6e4d470cf3e9b7afc9b
floravel <flo-hess1@web.de>
2021-01-24 10:28:32 +0100

    uncomment tests

--- tests/WeightTest.php
+++ tests/WeightTest.php
@@ -11,11 +11,10 @@ class WeightTest extends TestCase
     /** @test */
     public function it_can_convert_kilograms_to_lbs()
     {
-//        $lbs = Weight::fromKilograms(100)->toLbs();
+        $lbs = Weight::fromKilograms(100)->toLbs();
         $weight = Weight::fromKilograms(200);
-        var_dump($weight);
         $this->assertIsObject($weight);
-//        $this->assertEquals(220.4623, $lbs);
-//        $this->assertEquals(440.9246, $weight->toLbs());
+        $this->assertEquals(220.4623, $lbs);
+        $this->assertEquals(440.9246, $weight->toLbs());
     }
 }
